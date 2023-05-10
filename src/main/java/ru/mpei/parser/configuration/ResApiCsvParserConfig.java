5bec84bd848e3c72468f890a4a91fa7279411acf
Danila Paramonov <99509881+skrip04ka@users.noreply.github.com>
2023-05-10 16:00:05 +0300

    Delete src/main directory

--- src/main/java/ru/mpei/parser/configuration/ResApiCsvParserConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.mpei.parser.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.mpei.parser")
-public class ResApiCsvParserConfig {
-}
